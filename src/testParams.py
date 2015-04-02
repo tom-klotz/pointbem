dbfc119210f9774823f86fc73de3766cc9d89264
Matthew G. Knepley <knepley@gmail.com>
2015-04-02 16:19:05 -0500

    Added test architecture and some tests

---
+++ src/testParams.py
@@ -0,0 +1,12 @@
+regressionParameters = {'../../biology/pointbem-petsc/src/testSrfOnSurfacePoints':
+                            [{'numProcs': 1, 'args': '-num_charges 1 -nmax 25'},
+                             {'numProcs': 1, 'args': '-num_charges 10 -nmax 25'},
+                             {'numProcs': 1, 'args': '-num_charges 10 -nmax 25 -srf_num 2 -density 2.0'},
+                             {'numProcs': 1, 'args': '-num_charges 10 -nmax 25 -srf_num 3 -density 3.0'},
+                             {'numProcs': 1, 'args': '-num_charges 10 -nmax 25 -srf_num 4 -density 4.0'},
+                             {'numProcs': 1, 'args': '-num_charges 10 -nmax 25 -srf_num 5 -density 5.0'},
+                             {'numProcs': 1, 'args': '-num_charges 10 -nmax 25 -srf_num 6 -density 6.0'},
+                             {'numProcs': 1, 'args': '-num_charges 10 -nmax 25 -srf_num 8 -density 8.0'},
+                             {'numProcs': 1, 'args': '-num_charges 10 -nmax 25 -srf_num 10 -density 10.0'},
+                             ],
+                        }
