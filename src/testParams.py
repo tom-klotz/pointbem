30008bf5a68e59ef59c8d747602f2eac35fb5e21
Matthew G. Knepley <knepley@gmail.com>
2018-07-02 15:10:40 -0500

    Fixed test parameter default

--- src/testParams.py
+++ src/testParams.py
@@ -1,7 +1,9 @@
 import os
-regressionParameters = {os.environ.get("PETSC_TO_POINTBEM", ".")+'/src/testSrfOnSurfacePoints':
+petscToPointbem = os.path.relpath(os.path.abspath('..'), os.environ['PETSC_DIR'])
+print petscToPointbem
+regressionParameters = {os.environ.get("PETSC_TO_POINTBEM", petscToPointbem)+'/src/testSrfOnSurfacePoints':
                             [# Sphere tests [sphere_0-sphere_10]
-                             #{'num': 'sphere_0',  'numProcs': 1, 'args': '-srf_base ../geometry/sphere_R6_vdens0 -num_charges 10 -nmax 25 -srf_num 0125 -density 0.125 -ksp_rtol 1.0e-10 -snes_linesearch_type basic -snes_type ksponly'},
+                             {'num': 'sphere_0',  'numProcs': 1, 'args': '-srf_base ../geometry/sphere_R6_vdens0 -num_charges 10 -nmax 25 -srf_num 0125 -density 0.125 -ksp_rtol 1.0e-10 -snes_linesearch_type basic -snes_type ksponly'},
                              #{'num': 'sphere_1',  'numProcs': 1, 'args': '-srf_base ../geometry/sphere_R6_vdens0 -num_charges 10 -nmax 25 -srf_num 025 -density 0.25 -ksp_rtol 1.0e-10 -snes_linesearch_type basic -snes_type ksponly'},
                              #{'num': 'sphere_2',  'numProcs': 1, 'args': '-srf_base ../geometry/sphere_R6_vdens0 -num_charges 10 -nmax 25 -srf_num 05 -density 0.5 -ksp_rtol 1.0e-10 -snes_linesearch_type basic -snes_type ksponly'},
                              #{'num': 'sphere_3',  'numProcs': 1, 'args': '-srf_base ../geometry/sphere_R6_vdens  -num_charges 10 -nmax 25 -srf_num 1 -density 1.0 -ksp_rtol 1.0e-10 -snes_linesearch_type basic -snes_type ksponly'},
